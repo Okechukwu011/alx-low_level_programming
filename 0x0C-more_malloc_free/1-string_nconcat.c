2705c6fa17d243f34dfc7f7cc53eea1ae4d555a9
Okechukwu011 <godwinpeter963@gmail.com>
2023-09-07 08:48:02 -0700

    alx

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,44 @@
 #include "main.h"
-#include "stdio.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings
- * @s1: destination string
- * @s2: source string
- * @n: number of characters to be copied
- * Return: copied
+ * string_nconcat - function that concatenates two strings.
+ *
+ * @s1: This is the first input string
+ * @s2: This is the second input string
+ * @n: This is the limit to copy the second string to the first string
+ *
+ * Return: This is the string joined
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len = 0, s2_len = 0, i;
-	char *s;
+	unsigned int a, b;
+	char *newString;
 
-	/* if NULL is passed, treat as an empty string */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	/* For index = 0 */
-	for (s1[s1_len] != '\0', s1_len++)
-		;
-	for (s2[s2_len] != '\0', s2_len++)
-		;
-
-	if (n >= s2_len)
-		n = s2_len;
-
-	/* Memory allocation */
-	s = malloc(sizeof(char) * n + s1_len + 1);
-	if (s == NULL)
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+	}
+	if (n > b)
+		n = b;
+	newString = malloc(((a + n) + 1));
+	if (newString == NULL)
 		return (NULL);
-
-	for (i = 0; i < s1_len; i++, (s[i] = s1[i]))
-		;
-
-	for (i = 0; i < n; i++, (s[i + s1_len] = s2[i]))
-		;
-
-	s[i + s1_len] = '\0';
-
-	return (s);
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+		newString[a] = s1[a];
+	}
+	for (b = 0; b != n; b++)
+	{
+		newString[a] = s2[b];
+		a++;
+	}
+	newString[a] = '\0';
+	return (newString);
 }
