856a276ff6254cf5fd9b01be0d08f46d5061b73c
Okechukwu011 <godwinpeter963@gmail.com>
2023-09-20 06:24:22 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+
+/**
+ * main - main block
+ * Description: Print zero
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	printf("%d is zero", n);
+		return (0);
+}
