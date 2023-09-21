84a42bf572b4572f5227211bb3dcd37ee6d9fc4e
Okechukwu011 <godwinpeter963@gmail.com>
2023-09-21 08:12:31 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,19 @@
-#include <stddef.h>
 #include "function_pointers.h"
+
 /**
- * array_iterator - the function that takes in different
- * function to work on it
- * @array: the array to work on
- * @size: size of the array
- * @action: the operation to perform on it
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array.
+ * @array: input integer array.
+ * @size: size of the array.
+ * @action: pointer to the function.
+ *
+ * Return: no return.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array != NULL && size > 0 && action != NULL)
+	if (array && action)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
-
 /**
-* get_op_func - function pointer that selects the correct function to perform
-* the operation asked by the user
-* @s: the operator given by the user
-*
-* Return: pointer to the function that corresponds to the
-* operator given as a parameter
-*/
+ * get_op_func - identify function
+ * @s: funct
+ *
+ * Return: int
+ *
+ **/
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -21,13 +21,15 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (s[0] == ops[i].op[0])
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
+
 
 	return (NULL);
 }
