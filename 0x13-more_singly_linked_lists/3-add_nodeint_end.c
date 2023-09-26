862cf4737c36a0d8297f4065645409d85d92488a
Okechukwu011 <godwinpeter963@gmail.com>
2023-09-26 04:18:12 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new = (listint_t *)malloc(sizeof(listint_t));
 
 	listint_t *current;
-	current= *head;
+
+	current = *head;
 
 	if (new == NULL)
 	{
